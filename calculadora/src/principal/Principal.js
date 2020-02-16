2aa95a8b3c50c0ac5dcb5926d36150c368ce3a55
WellingtonMiarro <whellingtonmiarroferreira@gmail.com>
2020-02-16 02:51:43 -0400

    Calculadora

---
+++ calculadora/src/principal/Principal.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './Principal.css';
+
+export default class Principal extends Component{
+    render(){
+        return(
+            <div className='principal'></div>
+        )
+    }
+}
