057351f9af3f4b6e8fa688b9ee6d82763097fc2a
WellingtonMiarro <whellingtonmiarroferreira@gmail.com>
2020-03-21 00:35:20 -0400

    Exemplo usando JAVA

---
+++ TodoListApp/src/main/java/ifms/aula/Todo.java
@@ -0,0 +1,34 @@
+package ifms.aula;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "javaTodo")
+public class Todo {
+	
+	@Id
+	private String id;
+	
+	private String descricao;
+	
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
