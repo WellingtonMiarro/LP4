08337c9508630f26c9c44364207b5e52652e1415
WellingtonMiarro <whellingtonmiarroferreira@gmail.com>
2020-04-14 16:55:01 -0400

    Finalizando as Videos Aula - Quarentena

--- TUDO/Frontend/webpack.config.js
+++ TUDO/Frontend/webpack.config.js
@@ -1,40 +1,40 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtactTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: '../src/index.jsx',
+    entry: './src/index.jsx',
     output: {
-    path: __dirname + '/public', 
-    filename: './app.js'
+        path: __dirname + 'public',
+        filename: './app.js'
     },
-    devServer:{
+    devServer: {
         port: 8080,
-        contentBase:'/public',
+        contentBase: './public'
     },
-    resolve:{
+    resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            module: __dirname + '/node_modules'
+            modules: __dirname + '/node_modules'
         }
     },
-    plugins: {
-        new: ExtractTextPlugin('app.css')
-    },
-    module:{
+    plugins: [
+        new ExtactTextPlugin('app.css')
+    ],
+    module: {
         loaders: [{
-            test:/.js[x]?$/,
+            test: /.js[x]?$/, 
             loader: 'babel-loader',
             exclude: /node_modules/,
-            query:{
-                presets: ['es2015','react'],
-                plugins:['transform-object-rest-spread']
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader','css-loader')
-        },{
-            test: /\.woff|woff2|.ttf|.eot|.svg*.*/,
+            loader: ExtactTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
-    }   
+    }
 }
