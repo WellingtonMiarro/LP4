70b344faf11e54a97a9d462f52d4042dfdd6e2d3
WellingtonMiarro <whellingtonmiarroferreira@gmail.com>
2020-04-27 16:33:26 -0400

    Aula do dia 27/04 a 01/05

--- TUDO/Backend/src/api/todo/todo.js
+++ TUDO/Backend/src/api/todo/todo.js
@@ -2,16 +2,16 @@ const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
 const todoSchema = new mongoose.Schema({
-    descricao:{
+    description:{
         type: String,
         required: true
     },
-    finalizado:{
+    done:{
         type:Boolean,
         required: true,
         default:false
     },
-    criadoEm:{
+    createdAt:{
         type: Date,
         default: Date.now
     }
--- TUDO/Backend/src/config/app.js
+++ TUDO/Backend/src/config/app.js
@@ -5,9 +5,11 @@ const express = require('express');
 
 //Server ou App 
 const app = express();
+const allowdCors = require('./cors')
 
 app.use(bodyParser.urlencoded({extended: true})); // vai ler a url e vai tranformar em um obj JSON
 app.use(bodyParser.json());
+app.use(allowdCors);
 
 app.listen(port, () => console.log("Aplicação rodando na porta 3001"));
 
